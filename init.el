904797f167835339fe03c268ad15d15c82b24af9
Kulin <amiablechief@outlook.com>
2021-01-24 21:12:38 -0500

    Initial commit

---
+++ init.el
@@ -0,0 +1,66 @@
+(setq inhibit-startup-message t) ;; Disable startup modes
+
+(set scroll-bar-mode -1) ;; Disable scrollbars
+(tool-bar-mode -1) ;; Disable toolbars
+(tooltip-mode -1) ;; Disable tool tips
+(set-fringe-mode 10) ;; Give more breathing room
+
+(menu-bar-mode -1)  ;; Disable the menu bar
+
+;; Disable audible bell and set visible bell instead
+(setq visible-bell t)
+
+;; Set a reasonable font and font size
+(set-face-attribute 'default nil :font "JetBrains Mono" :height 140)
+
+;; Set a theme
+(load-theme 'wombat)
+
+;; Make ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+;; Initialize package sources
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ;;("melpa-stable" . "https://stable.melpa.org/packages/")
+			 ("org" . "https://orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(use-package command-log-mode)
+
+(use-package counsel)
+
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+	 :map ivy-minibuffer-map
+	 ("TAB" . ivy-alt-done)
+	 ("C-l" . ivy-alt-done)
+	 ("C-j" . ivy-next-line)
+	 ("C-k" . ivy-previous-line)
+	 :map ivy-switch-buffer-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-l" . ivy-done)
+	 ("C-d" . ivy-switch-buffer-kill)
+	 :map ivy-reverse-i-search-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 15)))
